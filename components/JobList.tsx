300dd2567ffbdc283d03308c8e68a88dea5c78b0
serquin35 <negociosenred3.0@gmail.com>
2025-12-31 19:24:04 +0100

    fix: move delete button inside card and update instructions for synchronous n8n flow

--- components/JobList.tsx
+++ components/JobList.tsx
@@ -122,13 +122,13 @@ export default function JobList({ initialJobs }: JobListProps) {
                             )}
                         </Link>
 
-                        {/* Delete Button - Fixed Position */}
+                        {/* Delete Button - Fixed Position Inside Card */}
                         <button
                             onClick={(e) => handleDelete(e, job.id)}
-                            className="absolute top-1/2 -translate-y-1/2 -left-12 p-3 text-gray-500 hover:text-red-500 opacity-0 group-hover:opacity-100 transition-all hover:scale-110 z-10"
+                            className="absolute top-2 right-2 p-2 text-gray-400 hover:text-red-500 hover:bg-red-500/10 rounded-lg opacity-0 group-hover:opacity-100 transition-all z-20"
                             title="Eliminar anuncio"
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                             </svg>
                         </button>
